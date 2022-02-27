0c45877c237026ad916bca3d2a78599b805483ac
yosou20 <yosou20@student.sdu.dk>
2022-02-27 15:25:41 +0100

    change test services1

--- test_services.sh
+++ test_services.sh
@@ -7,10 +7,10 @@
 kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/GatewayService/main/deployment.yaml
 
 # CRUD SERVICE
-# kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/CRUDservice/main/deployment.yaml
+ kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/CRUDservice/main/deployment.yaml
 
 # SOLVER INFO SERVICE
-# kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/SolverInfoService/main/deployment.yaml
+ kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/SolverInfoService/main/deployment.yaml
 
 # AUTH SERVICE
 kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/AuthenticationService/main/deployment.yaml
@@ -19,7 +19,7 @@ kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/Authenticati
 # kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/LoggingService/main/deployment.yaml
 
 # JOB SERVICE
-# kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/JobService/main/deployment.yaml
+ kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/JobService/main/deployment.yaml
 
 # MINIZINC SERVICE
-# kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/MiniZincService/main/deployment.yaml
+ kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/MiniZincService/main/deployment.yaml
